e326defdf93b3425477ace54105396f999d6cfcf
alvahearth <tekken5lord@gmail.com>
2020-07-15 19:07:18 -0400

    python

--- python/hola.py
+++ python/hola.py
@@ -1,13 +1,3 @@
-palabra = "hola"
-largo = len(palabra)
-print(largo)
-palabra_1 = palabra[::-1]
-print(palabra_1)
-count = 0
-
-for char in palabra_1:
-    count = count + 1
-    print(count, char)
-    
-    
-    
+palabra = "mortal kombat"
+alreves = palabra[::-1]
+print(alreves)
