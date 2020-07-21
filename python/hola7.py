cd587ef466a7fb0427ea97feb5a3d6a602a19a8b
alvahearth <tekken5lord@gmail.com>
2020-07-21 18:41:11 -0400

    python

--- python/hola7.py
+++ python/hola7.py
@@ -1,5 +1,18 @@
-fhand = open("mbox-short.txt")
-for line in fhand:
-    line = line.upper()
-    line = line.rstrip()
-    print(line)
+# Use the file name mbox-short.txt as the file name
+fname = input("Enter file name: ")
+fh = open(fname)
+count = 0
+average = 0
+for line in fh:
+    if line.startswith("X-DSPAM-Confidence:") :
+        count = count + 1
+        linex = line[19:]
+        floating = float(linex)
+        #print(floating)
+        average = average + floating
+            
+
+final = average / count  
+
+
+print("Average spam confidence:",final)
