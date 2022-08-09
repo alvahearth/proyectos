86009c4ffb1de168feb753645835c612d8282080
alvahearth <tekken5lord@gmail.com>
2022-08-09 01:39:41 -0400

    Nueva versión

--- python/python_django/app_banco/app_banco/settings.py
+++ python/python_django/app_banco/app_banco/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'h4xi88e5d_3!2c-j8e8vf61k)wwwxfd9(bm=*p#czpz#k4th2k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
